a0989bd45685665ec191adb0695690b1c70aae73
kedoo lightbody <35748718+kedoolightbody@users.noreply.github.com>
2018-05-01 19:13:50 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+alert('hello');
+
+var elem = document.querySelector('.materialboxed');
+  var instance = M.Materialbox.init(elem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
